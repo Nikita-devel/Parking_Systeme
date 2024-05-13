965afa5274f6bce87c5a9817887992cc38574a38
serhiikhkr <05hello.world11@gmail.com>
2024-05-13 03:12:12 +0300

    add more routes and rep

--- src/routes/parking.py
+++ src/routes/parking.py
@@ -6,7 +6,8 @@ from typing import Union
 
 from src.database.db import get_db
 from src.database.models import User, Role
-from src.schemas.parking import ParkingRateSchema, ParkingBalanceLimitSchema, SessionInfo, MessageResponse
+from src.schemas.parking import ParkingRateSchema, ParkingBalanceLimitSchema, SessionInfoSchema, MessageResponse, \
+    PlateSchema
 from src.services.auth import auth_service
 from src.conf.config import config
 from src.repository import users as repositories_users
@@ -45,14 +46,13 @@ async def set_balance_limit(balance_limit: int, db: AsyncSession = Depends(get_d
                                 detail="Недопустимая стоимость. Стоимость должна быть неотрицательной.")
 
 
-@router.get("/search_plate/{plate}", response_model=Union[list[SessionInfo], MessageResponse])
+@router.get("/search_plate/{plate}", response_model=Union[list[SessionInfoSchema], MessageResponse])
 async def searching_plate(plate: str, db: AsyncSession = Depends(get_db),
                           current_user: User = Depends(auth_service.get_current_user)):
     if not current_user.is_admin:
         raise HTTPException(status_code=403, detail="Permission denied")
     else:
         plate_info = await repositories_parking.search_plate(plate, db)
-
         if plate_info:
             plate_sessions = await repositories_parking.get_sessions_for_plate(plate_info.id, db)
 
@@ -64,4 +64,30 @@ async def searching_plate(plate: str, db: AsyncSession = Depends(get_db),
 
         else:
             return MessageResponse(message="Plate is not registered")
+
+
+@router.post("/add_new_plate/{plate}", response_model=PlateSchema)
+async def add_new_plate(plate: str, db: AsyncSession = Depends(get_db),
+                        current_user: User = Depends(auth_service.get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=403, detail="Permission denied")
+
+    else:
+        new_plate_to_bd = await repositories_parking.adding_new_plate(plate, db=db)
+
+        if new_plate_to_bd is None:
+            raise HTTPException(status_code=409, detail="This plate is already registered")
+
+        return PlateSchema(id=new_plate_to_bd.id, plate=new_plate_to_bd.plate, user_id=new_plate_to_bd.user_id)
+
+
+@router.post("/remove_plate/{plate}", response_model=MessageResponse)
+async def remove_plate(plate: str, db: AsyncSession = Depends(get_db),
+                        current_user: User = Depends(auth_service.get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=403, detail="Permission denied")
+
+    else:
+        result_delete_plate = await repositories_parking.delete_plate(plate, db)
+        return MessageResponse(message=result_delete_plate)
 
--- src/schemas/parking.py
+++ src/schemas/parking.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
 from src.database.models import Role
+from typing import Optional
 
 
 class ParkingRateSchema(BaseModel):
@@ -11,7 +12,7 @@ class ParkingBalanceLimitSchema(BaseModel):
     balance_limit: int = Field(ge=1, le=5000)
 
 
-class SessionInfo(BaseModel):
+class SessionInfoSchema(BaseModel):
     entrance_time: datetime = Field()
     exit_time: datetime = Field()
     owner: str = Field()
@@ -22,3 +23,9 @@ class SessionInfo(BaseModel):
 
 class MessageResponse(BaseModel):
     message: str
+
+
+class PlateSchema(BaseModel):
+    id: int
+    plate: str
+    user_id: Optional[int]
