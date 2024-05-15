08c523b26efa4565ca9b3b43bb9e3bab6d773bbf
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2024-05-15 20:51:09 +0300

    Add files via upload

--- src/conf/config.py
+++ src/conf/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_URL: str = "postgresql+asyncpg://postgres:goit1212@localhost:5432/postgres"
+    DB_URL: str = "postgresql+asyncpg://postgres:02012009@localhost:5432/postgres"
     SECRET_KEY_JWT: str = "974790aec4ac460bdc11645decad4dce7c139b7f2982b7428ec44e886ea588c6"
     ALGORITHM: str = "HS256"
     MAIL_USERNAME: str = "tryfastapi@meta.ua"
--- src/routes/parking_services.py
+++ src/routes/parking_services.py
@@ -1,3 +1,4 @@
+import cv2
 from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -13,18 +14,57 @@ from src.conf.config import config
 from src.repository import users as repositories_users
 from src.repository import parking as repositories_parking
 from src.repository import parking_services as repositories_parking_services
+from src.neural_network.neural_net import get_num_avto
 
 router = APIRouter(prefix="/parking_services", tags=["parking services"])
 
 
-@router.post("/entrance", response_model=SessionInfoSchema)
-async def entrance(plate: str, db: AsyncSession = Depends(get_db),
+@router.post("/entrance", response_model=Union[SessionInfoSchema, MessageResponse])
+async def entrance(file: UploadFile = File(...), db: AsyncSession = Depends(get_db),
                    current_user: Optional[User] = Depends(auth_service.get_current_user)):
-    check_plate = await repositories_parking.search_plate(plate, db)
+    contents = await file.read()
+    with open(f"./img/{file.filename}", "wb") as f:
+        f.write(contents)
+
+    # Завантаження зображення
+    original = cv2.imread(f"./img/{file.filename}")
+
+    # Розпізнавання номерного знаку
+    plate_number, _ = get_num_avto(original)
+    check_plate = await repositories_parking.search_plate(plate_number, db)
     if check_plate is None:
-        check_plate = await repositories_parking.adding_new_plate(plate, current_user.id, db)
+        check_plate = await repositories_parking.adding_new_plate(plate_number, current_user.id, db)
 
     new_session = await repositories_parking_services.entrance_for_authorized(check_plate, db)
-    return SessionInfoSchema(entrance_time=new_session.entrance_time, exit_time=new_session.exit_time,
-                             total_hours_spent=new_session.total_hours_spent, total_coast=new_session.total_coast,
-                             payment=new_session.payment)
+
+    if type(new_session) == str:
+        return MessageResponse(message=new_session)
+    else:
+        return SessionInfoSchema(entrance_time=new_session.entrance_time, exit_time=new_session.exit_time,
+                                total_hours_spent=new_session.total_hours_spent, total_coast=new_session.total_coast,
+                                payment=new_session.payment)
+
+@router.post("/exit", response_model=Union[SessionInfoSchema, MessageResponse])
+async def exit(file: UploadFile = File(...), db: AsyncSession = Depends(get_db),
+                   current_user: Optional[User] = Depends(auth_service.get_current_user)):
+    contents = await file.read()
+    with open(f"./img/{file.filename}", "wb") as f:
+        f.write(contents)
+
+    # Завантаження зображення
+    original = cv2.imread(f"./img/{file.filename}")
+
+    # Розпізнавання номерного знаку
+    plate_number, _ = get_num_avto(original)
+    check_plate = await repositories_parking.search_plate(plate_number, db)
+    if check_plate is None:
+        return MessageResponse(message="Check the license plate number")
+
+    close_session = await repositories_parking_services.exit(check_plate, db)
+
+    if type(close_session) == str:
+        return MessageResponse(message=close_session)
+
+    return SessionInfoSchema(entrance_time=close_session.entrance_time, exit_time=close_session.exit_time,
+                             total_hours_spent=close_session.total_hours_spent, total_coast=close_session.total_coast,
+                             payment=close_session.payment)
